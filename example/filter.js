f8aca1572fc749d3c62bde333630af3bd33af7c0
Michael Nisi <michael.nisi@gmail.com>
2013-04-27 19:38:12 +0200

    Improve examples

---
+++ example/filter.js
@@ -0,0 +1,23 @@
+
+// filter - cop properties by name
+
+var cop = require('../')
+  , Readable = require('stream').Readable
+
+var objs = [
+  { thing: 'My hovercraft ' }
+, { thing: 'is full ' }
+, { thing: 'of eels.\n' }
+]
+
+var reader = new Readable({ objectMode:true })
+  , length = objs.length
+  , i = 0
+
+reader._read = function () {
+  reader.push(i < length ?  objs[i++] : null)
+}
+
+reader
+  .pipe(cop('thing'))
+  .pipe(process.stdout)
